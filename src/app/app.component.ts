012b4ce9f0a9779c83100f1422ba83738a079d6c
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-24 15:27:12 -0500

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,45 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit {
   tareas: any[] = [];
+  tarea = {
+    id: null,
+    nombre: '',
+    completado: false,
+  };
 
   constructor(private appService: AppService) {}
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
     this.appService.getAll().subscribe((data: any) => (this.tareas = data));
+  }
+
+  save() {
+    if (this.tarea.id) {
+      this.appService
+        .update(this.tarea.id!, this.tarea)
+        .subscribe(() => this.getAll());
+    } else {
+      this.appService.create(this.tarea).subscribe(() => this.getAll());
+    }
+
+    this.tarea = {
+      id: null,
+      nombre: '',
+      completado: false,
+    };
+  }
+
+  edit(tarea: any) {
+    this.tarea = {
+      ...tarea,
+    };
+  }
+
+  delete(tarea: any) {
+    this.appService.delete(tarea.id).subscribe(() => this.getAll());
   }
 }
